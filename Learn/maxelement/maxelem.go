3d02ae9a8479c6d1f91ad64a0af2cc80f8fbc923
Nitesh Oswal <nit.oswal@gmail.com>
2015-11-11 17:26:27 +0530

    Find max element Go version - things I learnt - Pass slices not arrays!

---
+++ Learn/maxelement/maxelem.go
@@ -0,0 +1,28 @@
+package main
+import (
+    "fmt"
+)
+
+func max_element_position(a []int, size int) (int, int) {
+    positionOfMaxElement := 0
+    if size < 1 { return -1, -1 }
+    for i := 0; i < size; i++ {
+        if a[positionOfMaxElement] < a[i] {
+            positionOfMaxElement = i
+        }
+    }
+    return positionOfMaxElement, a[positionOfMaxElement]
+}
+
+func main() {
+    var size, pos, value, i int
+    var a [10] int
+    fmt.Scanf("%d", &size)
+    for i = 0; i < size; i++ {
+        fmt.Scanf("%d", &a[i])
+    }
+    pos, value = max_element_position(a[:], size)
+    if pos != -1 {
+        fmt.Printf("%d\n%d", pos, value);
+    }
+}
